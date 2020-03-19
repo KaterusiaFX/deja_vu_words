1ad1128cee9f93b6dcffa7205993f44151f8f8b5
KaterusiaFX <rusik888@gmail.com>
2020-03-19 10:35:48 +0300

    user admin created, login - logout function created

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask import flash, redirect, url_for
 
-from webapp.model import db
+from flask_login import LoginManager, login_user, logout_user
+
+from webapp.model import db, User
 from webapp.forms import LoginForm
 from webapp.config import Config
 
@@ -11,17 +14,44 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/')
     @app.route('/index')
     def index():
         title = 'Домой'
-        return render_template('index.html', title=title)
+        return render_template('index.html', page_title=title)
 
     @app.route('/login')
     def login():
         title = 'Авторизация'
         login_form = LoginForm()
-        return render_template('login.html', title=title, form=login_form)
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
 
     return app
 
