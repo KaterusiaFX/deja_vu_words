5946bf7cdf0fe03da55621f2c4f93325ed78e254
KaterusiaFX <61011253+KaterusiaFX@users.noreply.github.com>
2020-03-21 11:56:53 +0300

    Feature web forms (#5)
    * feature web_forms first version
    
    * forms.py changed forms names
    
    * forms, routes, templates comments added
    
    * Перенесла config в папку app, убрала все комментарии из файлов
    
    * Перенесла routes в __init__.py, удалила routes.py, удалила deja_vu_words.py, переименовала папку app в webapp
    
    * Bootstrap elements added to login.html and base.html. DB created, user model for DB created
    
    * Delete config.py
    
    * config.py added to gitignore
    
    * user admin created, login - logout function created
    
    * url only for loged in admin user added
    
    * button login changes to logout when user is authenticated. Transfer template of alerts in base.html
    
    * заменила Аутентификация на Вход
    
    * deja_vu_database.db

---
+++ webapp/__init__.py
@@ -0,0 +1,69 @@
+from flask import Flask
+from flask import render_template
+from flask import flash, redirect, url_for
+
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+
+from webapp.model import db, User
+from webapp.forms import LoginForm
+from webapp.config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        title = 'Домой'
+        return render_template('index.html', page_title=title)
+
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = 'Вход'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return "Привет админ!"
+        else:
+            return "Ты не админ!"
+
+    return app
+
+
+
