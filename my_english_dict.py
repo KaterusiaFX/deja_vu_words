5671a43f31d760c9f97ba17f30c3c0794db9c1cc
Earlinn Drow <earlinndrow@gmail.com>
2020-03-26 14:53:38 +0300

    слова, добавляемые пользователями, попадают не в общий словарь, а в отдельный

--- my_english_dict.py
+++ my_english_dict.py
@@ -1,5 +1,5 @@
 from webapp import create_app
-from webapp.model import db, EnglishWord, User, users_words
+from webapp.model import db, EnglishWord, User, users_words, EnglishWordOfUser
 from datetime import datetime
 
 app = create_app()
@@ -7,19 +7,25 @@ app = create_app()
 
 def webapp_engdict_insert(word, user):
     with app.app_context():
+        word_exist_userdict = EnglishWordOfUser.query.filter(EnglishWordOfUser.word_itself == word).all()
+        if word_exist_userdict:
+            for user_word in word_exist_userdict:
+                if user_word.user == user:
+                    return f'Слово "{word}" уже есть в вашем словаре.'
         word_exist = EnglishWord.query.filter(EnglishWord.word_itself == word).first()
         if not word_exist:
             word_itself = word
             word_translation = input('''Этого слова нет в английском словаре.
 Чтобы добавить его в английский словарь - введите его перевод на русский: ''')
-            new_word = EnglishWord(
-                word_itself=word_itself, 
+            new_word = EnglishWordOfUser(
+                word_itself=word_itself,
+                user=user,
                 translation_rus=word_translation, 
                 imported_time=datetime.now()
                 )
             db.session.add(new_word)
             db.session.commit()
-            insertion = user_engdict_insert(word, user)
+            insertion = user_engdict_own_insert(word, user)
             return f'{insertion} и в словарь на сайте.'
         return user_engdict_insert(word, user)
         
@@ -29,6 +35,16 @@ def user_engdict_insert(word, user):
         word_exist = EnglishWord.query.filter(EnglishWord.word_itself == word).first()
         user_exist = User.query.filter(User.username == user).first()
         user_exist.english_words.append(word_exist)
+        db.session.add(user_exist)
+        db.session.commit()
+        return f'Слово "{word}" добавлено в ваш словарь'
+
+
+def user_engdict_own_insert(word, user):
+    with app.app_context():
+        word_exist = EnglishWordOfUser.query.filter(EnglishWordOfUser.word_itself == word).first()
+        user_exist = User.query.filter(User.username == user).first()
+        user_exist.user_english_words.append(word_exist)
         db.session.add(user_exist)
         db.session.commit()
         return f'Слово "{word}" добавлено в ваш словарь'
--- webapp/model.py
+++ webapp/model.py
@@ -8,7 +8,9 @@ db = SQLAlchemy()
 users_words = db.Table('users_words', 
     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
     db.Column('engword_id', db.Integer, db.ForeignKey('English_words.id')),
-    db.Column('frenchword_id', db.Integer, db.ForeignKey('French_words.id'))
+    db.Column('frenchword_id', db.Integer, db.ForeignKey('French_words.id')),
+    db.Column('user_engword_id', db.Integer, db.ForeignKey('English_words_added_by_users.id')),
+    db.Column('user_frenchword_id', db.Integer, db.ForeignKey('French_words_added_by_users.id'))
     )
 
 
@@ -21,6 +23,9 @@ class User(db.Model, UserMixin):
     role = db.Column(db.String(10), index=True)
     english_words = db.relationship('EnglishWord', secondary=users_words, backref='english_words')
     french_words = db.relationship('FrenchWord', secondary=users_words, backref='french_words')
+    user_english_words = db.relationship('EnglishWordOfUser', secondary=users_words, backref='user_english_words')
+    user_french_words = db.relationship('FrenchWordOfUser', secondary=users_words, backref='user_french_words')
+
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -65,3 +70,34 @@ class FrenchWord(db.Model):
     def __repr__(self):
         return f'<Word "{self.word_itself}" in French language>'
 
+
+class EnglishWordOfUser(db.Model):
+    __tablename__ = 'English_words_added_by_users'
+    id = db.Column(db.Integer, primary_key=True)
+    word_itself = db.Column(db.String, nullable=False)
+    user = db.Column(db.String, nullable=False)
+    translation_rus = db.Column(db.String, nullable=False)
+    transcription = db.Column(db.String, nullable=True)
+    audio_url = db.Column(db.String, unique=True, nullable=True)
+    picture_url = db.Column(db.String, unique=True, nullable=True)
+    imported_time = db.Column(db.DateTime, nullable=False)
+    
+    def __repr__(self):
+        return f'<Word "{self.word_itself}" added by user {self.user} in English language>'
+
+
+class FrenchWordOfUser(db.Model):
+    __tablename__ = 'French_words_added_by_users'
+    id = db.Column(db.Integer, primary_key=True)
+    word_itself = db.Column(db.String, nullable=False)
+    user = db.Column(db.String, nullable=False)
+    translation_rus = db.Column(db.String, nullable=False)
+    transcription = db.Column(db.String, nullable=True)
+    feminine_or_masculine = db.Column(db.String, nullable=True)
+    french_verb_group = db.Column(db.String, nullable=True)
+    audio_url = db.Column(db.String, unique=True, nullable=True)
+    picture_url = db.Column(db.String, unique=True, nullable=True)
+    imported_time = db.Column(db.DateTime, nullable=False)
+    
+    def __repr__(self):
+        return f'<Word "{self.word_itself}" added by user {self.user} in French language>'
