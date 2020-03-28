aa0281991fd9e4fbb26e80ddea6365659a00ae9e
Earlinn Drow <earlinndrow@gmail.com>
2020-03-29 01:02:18 +0300

    Добавлен шаблон для отображения общих (не персональных) словарей - только для админов.

--- webapp/dictionary/views.py
+++ webapp/dictionary/views.py
@@ -8,5 +8,16 @@ blueprint = Blueprint('dictionary', __name__, url_prefix='/dictionary')
 @blueprint.route('/')
 @admin_required
 def dictionary_index():
-    title = "Общий словарь на сайте"
-    return render_template('dictionary/dictionary_index.html', page_title=title)
+    title = "Доступные словари сайта"
+    english_words = EnglishWord.query.order_by(EnglishWord.word_itself).all()
+    english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
+    french_words = FrenchWord.query.order_by(FrenchWord.word_itself).all()
+    french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
+    return render_template(
+        'dictionary/dictionary_index.html', 
+        page_title=title, 
+        english_list=english_words,
+        english_list_len=english_words_sum, 
+        french_list=french_words,
+        french_list_len=french_words_sum,
+        )
