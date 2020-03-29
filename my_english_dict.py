092f44d6115584bdc754e573c05624040eafe817
Earlinn Drow <earlinndrow@gmail.com>
2020-03-30 01:30:42 +0300

    В шаблоне menu.html добавлены кнопки, чтобы админ мог переходить на страницы с общим словарем и контентом админа, переписана программа my_english_dict - добавлены проверки существования пользователя с таким именем, статуса пользователя (админ или нет), для админов добавлены возможности менять первод слов в общем словаре и записывать в него новые слова, для обычного пользователя добавлена возможность вводить свой перевод слова, даже если оно есть в общем словаре (но этот перевод не попадает в общий словарь, он попадает только в специальный словарь с переводами пользователей. Прилагается схема работа программы my_english_dict.

--- my_english_dict.py
+++ my_english_dict.py
@@ -10,27 +10,83 @@ app = create_app()
 def webapp_engdict_insert(word, user):
     with app.app_context():
         word_exist_userdict = EnglishWordOfUser.query.filter(EnglishWordOfUser.word_itself == word).all()
+        
+        # проверка существования пользователя
+        user_exist = User.query.filter(User.username == user).first()
+        if not user_exist:
+            return f'Пользователь {user} не зарегистрирован на сайте.'
+
+        # слово есть в словаре пользователя
         if word_exist_userdict:
             for user_word in word_exist_userdict:
                 if user_word.user == user:
-                    return f'Слово "{word}" уже есть в вашем словаре.'
+                    return f'Слово "{word}" уже есть в вашем словаре, перевод: {user_word.translation_rus}.'
+
+        # слова нет в словаре пользователя
         word_exist = EnglishWord.query.filter(EnglishWord.word_itself == word).first()
+
+        # слова нет в общем словаре (словарь, генерируемый админами)
         if not word_exist:
-            word_itself = word
             word_translation = input('''Этого слова нет в английском словаре.
 Чтобы добавить его в английский словарь - введите его перевод на русский: ''')
-            new_word = EnglishWordOfUser(
-                word_itself=word_itself,
-                user=user,
-                translation_rus=word_translation, 
+            return admin_or_not(user, word, word_translation)
+                    
+        # слово есть в общем словаре, спрашиваем, устраивает ли пользователя перевод слова
+        dict_translation = word_exist.translation_rus
+        other_translation = input(f'Перевод: {dict_translation}. Изменить перевод слова (Y/N)? ')
+        
+        # пользователь согласен с предложенным вариантом перевода
+        if other_translation == 'N':
+            insertion = user_engdict_insert(word, user)
+            return f'{insertion}, перевод: {dict_translation}.'
+        
+        # пользователь не согласен с предложенным вариантом перевода и хочет добавить собственный перевод
+        if other_translation == 'Y':
+            word_translation = input('Введите свой вариант перевода: ')
+            return admin_or_not(user, word, word_translation)
+
+
+def admin_or_not(user, word, translation):
+    user_exist = User.query.filter(User.username == user).first()
+    word_exist = EnglishWord.query.filter(EnglishWord.word_itself == word).first()
+
+    # пользователь - не админ, слово добавляется в спецсловарь (словарь, генерируемый пользователями)
+    if not user_exist.is_admin:
+        user_new_word = EnglishWordOfUser(
+            word_itself=word,
+            user=user,
+            translation_rus=translation, 
+            imported_time=datetime.now()
+            )
+        db.session.add(user_new_word)
+        db.session.commit()
+        insertion = user_engdict_own_insert(word, user)
+        return f'{insertion} и в словарь на сайте, перевод: {translation}.'
+    
+    # слова нет в общем словаре (пользователь-админ), слово добавляется в общий словарь и в спецсловарь тоже
+    if not word_exist:
+        new_word = EnglishWord(
+                word_itself=word,
+                translation_rus=translation, 
                 imported_time=datetime.now()
                 )
-            db.session.add(new_word)
-            db.session.commit()
-            insertion = user_engdict_own_insert(word, user)
-            return f'{insertion} и в словарь на сайте.'
-        return user_engdict_insert(word, user)
-        
+        db.session.add(new_word)
+        user_new_word = EnglishWordOfUser(
+                word_itself=word,
+                user=user,
+                translation_rus=translation, 
+                imported_time=datetime.now()
+                )
+        db.session.add(user_new_word)
+        db.session.commit()
+        insertion = user_engdict_insert(word, user)
+        return f'{insertion} и в словарь на сайте, перевод: {translation}.' 
+    # слово есть в общем словаре и его надо заменить (пользователь-админ)
+    word_exist.translation_rus = translation
+    db.session.commit()
+    insertion = user_engdict_insert(word, user)
+    return f'{insertion}, перевод: {translation}.'
+
 
 def user_engdict_insert(word, user):
     with app.app_context():
