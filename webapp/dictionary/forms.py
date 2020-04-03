2c60065642197c644337cf8369e484ce9d3ab95d
Earlinn Drow <earlinndrow@gmail.com>
2020-04-04 01:33:11 +0300

    Добавлен поиск по английскому словарю на странице админа с использованием нового шаблона, выводящего результат поиска, для французского словаря на странице админа создан новый шаблон, т.е. французский словарь больше не выводится на той же странице, что и английский

---
+++ webapp/dictionary/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+from webapp.dictionary.models import EnglishWord
+
+
+class DictionarySearchForm(FlaskForm):
+    word = StringField('Поиск по английскому словарю', validators=[DataRequired()], render_kw={"class": "form-control"})
+    search = SubmitField('Поиск', render_kw={"class": "btn btn-info"})
+
+    def validate_word(self, word):
+        word = EnglishWord.query.filter_by(word_itself=word.data).first()
+        if word is None:
+            raise ValidationError('No such a word in our English dictionary')
--- webapp/dictionary/views.py
+++ webapp/dictionary/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, redirect, url_for
 
+from webapp.dictionary.forms import DictionarySearchForm
 from webapp.dictionary.models import EnglishWord, FrenchWord
 from webapp.user.decorators import admin_required
 
@@ -8,17 +9,33 @@ blueprint = Blueprint('dictionary', __name__, url_prefix='/dictionary')
 
 @blueprint.route('/')
 @admin_required
-def dictionary_index():
-    title = "Доступные словари сайта"
+def engdict_index():
+    title = "Английский словарь (в алфавитном порядке)"
+    search_form = DictionarySearchForm()
     english_words = EnglishWord.query.order_by(EnglishWord.word_itself).all()
     english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
-    french_words = FrenchWord.query.order_by(FrenchWord.word_itself).all()
-    french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
     return render_template(
-        'dictionary/dictionary_index.html',
+        'dictionary/engdict_index.html',
         page_title=title,
         english_list=english_words,
         english_list_len=english_words_sum,
-        french_list=french_words,
-        french_list_len=french_words_sum,
+        form=search_form
         )
+
+@blueprint.route('/process-engdict-search', methods=['POST'])
+def process_engdict_search():
+    title = "Английский словарь (в алфавитном порядке)"
+    search_form = DictionarySearchForm()
+    english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
+    if search_form.validate_on_submit():
+        word = EnglishWord.query.filter_by(word_itself=search_form.word.data).first()
+        return render_template(
+            'dictionary/engdict_search.html',
+            page_title=title,
+            english_word=word,
+            english_list_len=english_words_sum,
+            form=search_form
+            )
+
+    flash('Такого слова нет в нашем английском словаре')
+    return redirect(url_for('dictionary.engdict_index'))
