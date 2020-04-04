9a60bfb5a63e15ea7dc238b504254032665e0f5c
Earlinn Drow <earlinndrow@gmail.com>
2020-04-04 23:28:13 +0300

    В валидаторы для целей отладки добавлены принты, выводящие в консоль сообщения об ошибках

--- webapp/dictionary/forms.py
+++ webapp/dictionary/forms.py
@@ -6,20 +6,30 @@ from webapp.dictionary.models import EnglishWord, FrenchWord
 
 
 class EngDictionarySearchForm(FlaskForm):
-    word = StringField('Поиск по английскому словарю', validators=[DataRequired()], render_kw={"class": "form-control"})
+    word = StringField(
+        'Поиск по английскому словарю',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        )
     search = SubmitField('Поиск', render_kw={"class": "btn btn-info"})
 
     def validate_word(self, word):
         word = EnglishWord.query.filter_by(word_itself=word.data).first()
         if word is None:
+            print('Ошибка! Слова нет в английском словаре!')
             raise ValidationError('No such a word in our English dictionary')
 
 
 class FrenchDictionarySearchForm(FlaskForm):
-    word = StringField('Поиск по французскому словарю', validators=[DataRequired()], render_kw={"class": "form-control"})
+    word = StringField(
+        'Поиск по французскому словарю',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        )
     search = SubmitField('Поиск', render_kw={"class": "btn btn-info"})
 
     def validate_word(self, word):
         word = FrenchWord.query.filter_by(word_itself=word.data).first()
         if word is None:
+            print('Ошибка! Слова нет во французском словаре!')
             raise ValidationError('No such a word in our French dictionary')
