4d85e212f50e724b3ea2cbc31b032f03f1e8b601
Earlinn Drow <earlinndrow@gmail.com>
2020-04-07 00:55:46 +0300

    В словари админа добавлен поиск слов по их русским переводам при помощи выпадающих списков

--- webapp/dictionary/forms.py
+++ webapp/dictionary/forms.py
@@ -5,7 +5,16 @@ from wtforms.validators import DataRequired
 
 class EngDictionarySearchForm(FlaskForm):
     word = StringField(
-        'Поиск по английскому словарю',
+        'Поиск по английскому словарю на английском',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        )
+    search = SubmitField('Поиск', render_kw={"class": "btn btn-info"})
+
+
+class EngDictionarySearchFormRus(FlaskForm):
+    word = StringField(
+        'Поиск по английскому словарю на русском',
         validators=[DataRequired()],
         render_kw={"class": "form-control"}
         )
@@ -14,7 +23,16 @@ class EngDictionarySearchForm(FlaskForm):
 
 class FrenchDictionarySearchForm(FlaskForm):
     word = StringField(
-        'Поиск по французскому словарю',
+        'Поиск по французскому словарю на французском',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        )
+    search = SubmitField('Поиск', render_kw={"class": "btn btn-info"})
+
+
+class FrenchDictionarySearchFormRus(FlaskForm):
+    word = StringField(
+        'Поиск по французскому словарю на русском',
         validators=[DataRequired()],
         render_kw={"class": "form-control"}
         )
--- webapp/dictionary/views.py
+++ webapp/dictionary/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, flash, render_template
 
-from webapp.dictionary.forms import EngDictionarySearchForm, FrenchDictionarySearchForm
+from webapp.dictionary.forms import EngDictionarySearchForm, EngDictionarySearchFormRus
+from webapp.dictionary.forms import FrenchDictionarySearchForm, FrenchDictionarySearchFormRus
 from webapp.dictionary.models import EnglishWord, FrenchWord
 from webapp.user.decorators import admin_required
 from webapp.user.models import User
@@ -18,6 +19,24 @@ def engdict_index(username):
     english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
     return render_template(
         'dictionary/engdict_index.html',
+        page_title=title,
+        english_list=english_words,
+        english_list_len=english_words_sum,
+        form=search_form,
+        user=username
+        )
+
+
+@blueprint.route('/admin_engdict_rus/<username>')
+@admin_required
+def engdict_index_rus(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    search_form = EngDictionarySearchFormRus()
+    title = "Английский словарь (в алфавитном порядке)"
+    english_words = EnglishWord.query.order_by(EnglishWord.word_itself).all()
+    english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
+    return render_template(
+        'dictionary/engdict_index_rus.html',
         page_title=title,
         english_list=english_words,
         english_list_len=english_words_sum,
@@ -56,6 +75,37 @@ def process_engdict_search(username):
             )
 
 
+@blueprint.route('/process-engdict-search-rus/<username>', methods=['POST'])
+def process_engdict_search_rus(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    title = "Английский словарь (в алфавитном порядке)"
+    search_form = EngDictionarySearchFormRus()
+    english_words = EnglishWord.query.order_by(EnglishWord.word_itself).all()
+    english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
+    if search_form.validate_on_submit():
+        word = EnglishWord.query.filter_by(translation_rus=search_form.word.data).first()
+        print(word)
+        if word:
+            return render_template(
+                'dictionary/engdict_search_rus.html',
+                page_title=title,
+                english_word=word,
+                english_list_len=english_words_sum,
+                form=search_form,
+                user=username
+                )
+
+        flash('Такого слова нет в нашем английском словаре')
+        return render_template(
+            'dictionary/engdict_index_rus.html',
+            page_title=title,
+            english_list=english_words,
+            english_list_len=english_words_sum,
+            form=search_form,
+            user=username
+            )
+
+
 @blueprint.route('/admin_frenchdict/<username>')
 @admin_required
 def frenchdict_index(username):
@@ -66,6 +116,24 @@ def frenchdict_index(username):
     french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
     return render_template(
         'dictionary/frenchdict_index.html',
+        page_title=title,
+        french_list=french_words,
+        french_list_len=french_words_sum,
+        form=search_form,
+        user=username
+        )
+
+
+@blueprint.route('/admin_frenchdict_rus/<username>')
+@admin_required
+def frenchdict_index_rus(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    search_form = FrenchDictionarySearchFormRus()
+    title = "Французский словарь (в алфавитном порядке)"
+    french_words = FrenchWord.query.order_by(FrenchWord.word_itself).all()
+    french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
+    return render_template(
+        'dictionary/frenchdict_index_rus.html',
         page_title=title,
         french_list=french_words,
         french_list_len=french_words_sum,
@@ -96,6 +164,36 @@ def process_frenchdict_search(username):
         flash('Такого слова нет в нашем французском словаре')
         return render_template(
             'dictionary/frenchdict_index.html',
+            page_title=title,
+            french_list=french_words,
+            french_list_len=french_words_sum,
+            form=search_form,
+            user=username
+            )
+
+
+@blueprint.route('/process-frenchdict-search-rus/<username>', methods=['POST'])
+def process_frenchdict_search_rus(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    title = "Французский словарь (в алфавитном порядке)"
+    search_form = FrenchDictionarySearchFormRus()
+    french_words = FrenchWord.query.order_by(FrenchWord.word_itself).all()
+    french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
+    if search_form.validate_on_submit():
+        word = FrenchWord.query.filter_by(translation_rus=search_form.word.data).first()
+        if word:
+            return render_template(
+                'dictionary/frenchdict_search_rus.html',
+                page_title=title,
+                french_word=word,
+                french_list_len=french_words_sum,
+                form=search_form,
+                user=username
+                )
+
+        flash('Такого слова нет в нашем французском словаре')
+        return render_template(
+            'dictionary/frenchdict_index_rus.html',
             page_title=title,
             french_list=french_words,
             french_list_len=french_words_sum,
