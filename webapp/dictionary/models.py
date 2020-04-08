015ce6cdf0a808dd5f7de4d5ae192d1319f50ab4
Earlinn Drow <earlinndrow@gmail.com>
2020-04-08 22:42:32 +0300

    Добавлен персональный француский словарь с возможностью поиска слов

--- webapp/dictionary/models.py
+++ webapp/dictionary/models.py
@@ -13,7 +13,7 @@ class UsersWords(db.Model):
     user_frenchword_id = db.Column(db.Integer, db.ForeignKey('French_words_added_by_users.id'))
     imported_time = db.Column(db.DateTime, nullable=True, default=datetime.now())
     status = db.Column(db.String, nullable=True, default='new')
-    
+
     users = db.relationship('User', backref='users')
     english_words = db.relationship('EnglishWord', backref='english_words')
     french_words = db.relationship('FrenchWord', backref='french_words')
--- webapp/dictionary/views.py
+++ webapp/dictionary/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, flash, redirect, render_template, url_for
 import re
 
 from webapp.dictionary.forms import EngDictionarySearchForm, FrenchDictionarySearchForm
-from webapp.dictionary.models import EnglishWord, EnglishWordOfUser, FrenchWord, UsersWords
+from webapp.dictionary.models import EnglishWord, EnglishWordOfUser, FrenchWord, FrenchWordOfUser, UsersWords
 from webapp.user.decorators import admin_required
 from webapp.user.models import User
 
@@ -35,7 +35,7 @@ def process_engdict_search(username):
     title = "Английский словарь (в алфавитном порядке)"
     search_form = EngDictionarySearchForm()
     english_words = EnglishWord.query.order_by(EnglishWord.word_itself).all()
-    english_words_sum = len(EnglishWord.query.order_by(EnglishWord.word_itself).all())
+    english_words_sum = len(english_words)
     if search_form.validate_on_submit():
         word = search_form.word.data
         if re.match("[a-zA-Z]+", word):
@@ -84,7 +84,7 @@ def process_frenchdict_search(username):
     title = "Французский словарь (в алфавитном порядке)"
     search_form = FrenchDictionarySearchForm()
     french_words = FrenchWord.query.order_by(FrenchWord.word_itself).all()
-    french_words_sum = len(FrenchWord.query.order_by(FrenchWord.word_itself).all())
+    french_words_sum = len(french_words)
     if search_form.validate_on_submit():
         word = search_form.word.data
         if re.match("[a-zA-Z]+", word):
@@ -149,7 +149,7 @@ def user_process_engdict_search(username):
         word_in_form, word = search_form.word.data, None
         if re.match("[a-zA-Z]+", word_in_form):
             word_exist_userdict = EnglishWordOfUser.query.filter_by(word_itself=word_in_form).all()
-            word_exist = EnglishWord.query.filter_by(word_itself=word_in_form).first() 
+            word_exist = EnglishWord.query.filter_by(word_itself=word_in_form).first()
             for userword in user_words:
                 if word_exist and userword.engword_id == word_exist.id:
                     word = word_exist
@@ -185,4 +185,81 @@ def user_process_engdict_search(username):
         flash('Такого слова нет в вашем английском словаре')
         return redirect(url_for('.user_engdict_index', username=username.username))
 
-# добавить все то же самое для французского персонального словаря
+
+@blueprint.route('/user_frenchdict/<username>')
+def user_frenchdict_index(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    user_id = username.id
+    search_form = FrenchDictionarySearchForm()
+    title = "Ваш французский словарь"
+    user_words = UsersWords.query.filter_by(user_id=user_id).all()
+
+    french_words, french_words_status, french_words_date = [], [], []
+    for user_word in user_words:
+        if user_word.frenchword_id:
+            french_word = FrenchWord.query.filter_by(id=user_word.frenchword_id).first()
+        if user_word.user_frenchword_id:
+            french_word = FrenchWordOfUser.query.filter_by(id=user_word.user_frenchword_id).first()
+        french_words.append(french_word)
+        french_words_status.append(user_word.status)
+        french_words_date.append(user_word.imported_time)
+    french_list = list(zip(french_words, french_words_status, french_words_date))
+    french_words_sum = len(french_list)
+
+    return render_template(
+        'dictionary/user_frenchdict_index.html',
+        page_title=title,
+        french_list=french_list,
+        french_list_len=french_words_sum,
+        form=search_form,
+        user=username.username
+        )
+
+
+@blueprint.route('/user-process-frenchdict-search/<username>', methods=['POST'])
+def user_process_frenchdict_search(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    user_id = username.id
+    title = "Ваш французский словарь"
+    search_form = FrenchDictionarySearchForm()
+    user_words = UsersWords.query.filter_by(user_id=user_id).all()
+
+    if search_form.validate_on_submit():
+        word_in_form, word = search_form.word.data, None
+        if re.match("[a-zA-Z]+", word_in_form):
+            word_exist_userdict = FrenchWordOfUser.query.filter_by(word_itself=word_in_form).all()
+            word_exist = FrenchWord.query.filter_by(word_itself=word_in_form).first()
+            for userword in user_words:
+                if word_exist and userword.frenchword_id == word_exist.id:
+                    word = word_exist
+                    user_french_word_status, user_french_word_date = userword.status, userword.imported_time
+                elif word_exist_userdict:
+                    for every_word in word_exist_userdict:
+                        if every_word.user == username.username and every_word.id == userword.user_frenchword_id:
+                            word = every_word
+                            user_french_word_status, user_french_word_date = userword.status, userword.imported_time
+        elif re.match("[а-яА-Я]+", word_in_form):
+            word_exist_userdict = FrenchWordOfUser.query.filter_by(translation_rus=word_in_form).all()
+            word_exist = FrenchWord.query.filter_by(translation_rus=word_in_form).first()
+            for userword in user_words:
+                if word_exist and userword.frenchword_id == word_exist.id:
+                    word = word_exist
+                    user_french_word_status, user_french_word_date = userword.status, userword.imported_time
+                elif word_exist_userdict:
+                    for every_word in word_exist_userdict:
+                        if every_word.user == username.username and every_word.id == userword.user_frenchword_id:
+                            word = every_word
+                            user_french_word_status, user_french_word_date = userword.status, userword.imported_time
+        if word:
+            return render_template(
+                'dictionary/user_frenchdict_search.html',
+                page_title=title,
+                french_word=word,
+                french_word_status=user_french_word_status,
+                french_word_date=user_french_word_date,
+                form=search_form,
+                user=username.username
+                )
+
+        flash('Такого слова нет в вашем французском словаре')
+        return redirect(url_for('.user_frenchdict_index', username=username.username))
