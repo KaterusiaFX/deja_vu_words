7b279915d980d0492daddebeabfa903a033e5865
earlinn <41830050+earlinn@users.noreply.github.com>
2020-04-15 12:48:49 +0300

    Feature userdict (#11)
    * first commit
    
    * В словари админа добавлен поиск слов по их русским переводам при помощи выпадающих списков
    
    * Рефакторинг кода для поиска по русским словам в словаре админа - убраны лишние формы, шаблоны и функции, убраны dropdown lists из всех форм поиска
    
    * В табличку UsersWords добавлен столбец со временем импортирования, в профиль обычного пользователя добавлен вывод персонального словаря, а также поиск по нему, но при поиске некоторых слов возникает ошибка UnboundLocalError: local variable 'user_english_word_status' referenced before assignment (ссылка на строку 195 в webapp/dictionary/views.py)
    
    * Дебаг поиска по персональному английскому словарю, из шаблона поиска по этому словарю убрано общее количество слов в словаре, в dictiobary/views.py во всех поисковых функциях в случае отсутствия слова render_template заменен на redirect
    
    * Добавлен русскоязычный поиск по персональному английскому словарю, в БД в табличке UsersWords у колонки status значение параметра nullable изменено на True в целях избежания проблем с миграциями (вылезала ошибка, что не может быть создано поле со значением NULL)
    
    * Добавлен персональный француский словарь с возможностью поиска слов
    
    * Добавлено отображение пустых пользовательских словарей
    
    * Повторение ошибки с ModuleNotFoundError: No module named 'webapp'
    
    * Повтор ошибки с вынесением поиска слова за пределы views и закольцовкой импортов
    
    * Из dict_functions убран app и добавлена функция также для поиска французских слов в персональном словаре
    
    * Вынесены в отдельный файл функции поиска слов для вывода полных персональных словарей
    
    * Добавлена функция, предлагающая перевод слов, которых нет в персональном словаре
    
    * Реализовано добавление новых слов в персональный английский словарь в случаях, если поиск слова был на английском
    
    * Реализовано добавление слов в персональный французский словарь, доделано добавление слов в персональный английский словарь
    
    * Сделано наполнение общего французского словаря
    
    * Убрана кнопка отмены добавления слова в персональный словарь
    
    * Добавлена возможность удаления слов из персональных словарей (на странице поиска слова)
    
    * На странички поиска и вставки слов в персональные словари добавлена кнопка "назад в словарь"

--- get_french_words.py
+++ get_french_words.py
@@ -1,28 +1,26 @@
-from get_transcription import get_transcription
-from english_dict_generator import eng_dict_generator
+from french_dict_generator import french_dict_generator
 from datetime import datetime
 from webapp import create_app
 from webapp.db import db
-from webapp.dictionary.models import EnglishWord
+from webapp.dictionary.models import FrenchWord
 
 app = create_app()
 
 
 def save_words_in_db(words_dict):
     for word in words_dict:
-        word_exist = EnglishWord.query.filter(EnglishWord.word_itself == word).count()
+        word_exist = FrenchWord.query.filter(FrenchWord.word_itself == word).count()
         if not word_exist:
-            new_word = EnglishWord(
+            new_word = FrenchWord(
                 word_itself=word,
                 translation_rus=words_dict[word],
-                transcription=get_transcription(word),
                 imported_time=datetime.now()
                 )
             db.session.add(new_word)
     db.session.commit()
 
 
-words_dict = eng_dict_generator()
+words_dict = french_dict_generator()
 
 if __name__ == "__main__":
     with app.app_context():
