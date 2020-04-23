922f7aef3ca7c00a83f67c3870875a8ad9ced350
Earlinn Drow <earlinndrow@gmail.com>
2020-04-24 02:08:05 +0300

    Доделаны тренировки типа слово-перевод, у угаданных слов статус меняется на familiar и вписывается дата запоминания слова

--- webapp/exercises/exercise_functions.py
+++ webapp/exercises/exercise_functions.py
@@ -1,2 +1,35 @@
-def engword_translation(word):
-    pass
+from datetime import datetime
+
+from webapp.db import db
+from webapp.dictionary.dict_functions import user_engdict_search, user_frenchdict_search
+from webapp.dictionary.models import UsersWords
+
+
+def word_status_change_to_familiar(word):
+    word.status = 'familiar'
+    word.memorizing_time = datetime.now()
+    db.session.commit()
+
+
+def engword_translation_training(username, guess_word, user_answer):
+    guess_word = user_engdict_search(guess_word, username)
+    word = UsersWords.query.get(guess_word[3])
+    if guess_word[0].translation_rus == user_answer:
+        word.word_translation = None  # если слово изучено, то в ячейку пишется NULL
+        word_status_change_to_familiar(word)
+        return 'Верно', guess_word
+    word.word_translation += 1  # если пользователь ошибся с переводом, то увеличиваем счетчик попыток на 1
+    db.session.commit()
+    return 'Неверно', guess_word
+
+
+def frenchword_translation_training(username, guess_word, user_answer):
+    guess_word = user_frenchdict_search(guess_word, username)
+    word = UsersWords.query.get(guess_word[3])
+    if guess_word[0].translation_rus == user_answer:
+        word.word_translation = None  # если слово изучено, то в ячейку пишется NULL
+        word_status_change_to_familiar(word)
+        return 'Верно', guess_word
+    word.word_translation += 1  # если пользователь ошибся с переводом, то увеличиваем счетчик попыток на 1
+    db.session.commit()
+    return 'Неверно', guess_word
--- webapp/exercises/views.py
+++ webapp/exercises/views.py
@@ -3,6 +3,7 @@ from random import choice, random, sample
 import re
 
 from webapp.dictionary.dict_functions import process_user_engdict_index, process_user_frenchdict_index
+from webapp.exercises.exercise_functions import engword_translation_training, frenchword_translation_training
 from webapp.user.models import User
 
 blueprint = Blueprint('exercises', __name__, url_prefix='/exercises')
@@ -16,20 +17,60 @@ def choose_exercise(username):
 @blueprint.route('/engword_translation/<username>')
 def engword_translation(username):
     username = User.query.filter_by(username=username).first_or_404()
-    new_words = list(filter(lambda  x: x[1] == 'new', process_user_engdict_index(username)))
+    new_words = list(filter(lambda x: x[1] == 'new', process_user_engdict_index(username)))
     guess_word = choice(new_words)
     other_words = [word[0].translation_rus for word in new_words if word != guess_word]
     other_words = sample(other_words, 4)
     other_words.append(guess_word[0].translation_rus)
     mixture = sorted(other_words, key=lambda x: random())
-    return render_template('exercises/word_translation.html', guess_word=guess_word[0].word_itself, mixture=mixture)
+    return render_template(
+        'exercises/engword_translation.html',
+        guess_word=guess_word[0].word_itself,
+        mixture=mixture
+        )
 
 
-# убрать кнопку "Показать варианты", чтобы они были сразу развернуты
-@blueprint.route('/engword_translation_answer/<username>/<user_answer>/<guess_word>/<mixture>')
+@blueprint.route('/engword_translation_answer/<username>/<int:user_answer>/<guess_word>/<mixture>')
 def engword_translation_answer(username, user_answer, guess_word, mixture):
     username = User.query.filter_by(username=username).first_or_404()
     mixture = mixture.split(', ')
     mixture = [re.findall('[а-яА-Я]+', x) for x in mixture]
-    new_mixture = [mixture[0][0], mixture[1][0], mixture[2][0], mixture[3][0], mixture[4][0]]
-    return render_template('exercises/word_translation.html', guess_word=guess_word, mixture=new_mixture)
+    final_mixture = [' '.join(word) if len(word) > 1 else word[0] for word in mixture]
+    decision, true_word = engword_translation_training(username, guess_word, final_mixture[user_answer])
+    return render_template(
+        'exercises/engword_translation_answer.html',
+        true_word=true_word,
+        mixture=final_mixture,
+        decision=decision
+        )
+
+
+@blueprint.route('/frenchword_translation/<username>')
+def frenchword_translation(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    new_words = list(filter(lambda x: x[1] == 'new', process_user_frenchdict_index(username)))
+    guess_word = choice(new_words)
+    other_words = [word[0].translation_rus for word in new_words if word != guess_word]
+    other_words = sample(other_words, 4)
+    other_words.append(guess_word[0].translation_rus)
+    mixture = sorted(other_words, key=lambda x: random())
+    return render_template(
+        'exercises/frenchword_translation.html',
+        guess_word=guess_word[0].word_itself,
+        mixture=mixture
+        )
+
+
+@blueprint.route('/frenchword_translation_answer/<username>/<int:user_answer>/<guess_word>/<mixture>')
+def frenchword_translation_answer(username, user_answer, guess_word, mixture):
+    username = User.query.filter_by(username=username).first_or_404()
+    mixture = mixture.split(', ')
+    mixture = [re.findall('[а-яА-Я]+', x) for x in mixture]
+    final_mixture = [' '.join(word) if len(word) > 1 else word[0] for word in mixture]
+    decision, true_word = frenchword_translation_training(username, guess_word, final_mixture[user_answer])
+    return render_template(
+        'exercises/frenchword_translation_answer.html',
+        true_word=true_word,
+        mixture=final_mixture,
+        decision=decision
+        )
