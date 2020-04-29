cc942560e849d8c2d94b658aeed61b0a5103ed10
Earlinn Drow <earlinndrow@gmail.com>
2020-04-29 23:49:45 +0300

    Blueprint для графиков прогресса

---
+++ webapp/progress/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+from flask_login import current_user
+import locale
+import platform
+
+from webapp.user.views import check_teacher_student
+
+blueprint = Blueprint('progress', __name__, url_prefix='/progress')
+
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, 'russian')
+else:
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+
+@blueprint.route('/choose_diagram/<username>')
+def choose_diagram(username):
+    user_id = current_user.get_id()
+    user_status = check_teacher_student(user_id)
+    return render_template('diagram/choose_diagram.html', user_status=user_status)
+    # сделать шаблон для выбора
