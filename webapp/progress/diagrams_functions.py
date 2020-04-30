1f07d7388711908e89d7386abd8d5ccc9837e6c0
Earlinn Drow <earlinndrow@gmail.com>
2020-05-01 02:10:49 +0300

    Доделаны графики прогресса для английских слов

--- webapp/progress/diagrams_functions.py
+++ webapp/progress/diagrams_functions.py
@@ -17,35 +17,116 @@ else:
 def engword_dataframe(username):
     username = User.query.filter_by(username=username).first_or_404()
     userdict = process_user_engdict_index(username)
-    first_date = userdict[-1][2]
-    today = datetime.now()
-    delta = today - first_date
-    add_data = {'дни': [], 'добавленные слова за день': [], 'добавленные слова': []}
-    for day in range(delta.days + 2):
-        next_day = first_date + timedelta(days=day)
-        date = next_day.strftime('%d.%m.%Y')
-        add_data['дни'].append(date)
-        words_der_day = 0
-        for word in userdict:
-            if word[2].strftime('%d.%m.%Y') == date:
-                words_der_day += 1
-        add_data['добавленные слова за день'].append(words_der_day)
-        accumulated_words_added = sum(add_data['добавленные слова за день'])
-        add_data['добавленные слова'].append(accumulated_words_added)
-    return(add_data)
+    if userdict:
+        first_date = userdict[-1][2]
+        today = datetime.now()
+        delta = today - first_date
+        add_data = {
+            'дни': [],
+            'добавленные слова за день': [],
+            'добавленные слова': [],
+            'изученные слова за день': [],
+            'изученные слова': []
+            }
+        for day in range(delta.days + 2):
+            next_day = first_date + timedelta(days=day)
+            date = next_day.strftime('%d.%m.%Y')
+            add_data['дни'].append(next_day)
+            words_der_day, familiar_words_per_day = 0, 0
+            for word in userdict:
+                if word[2].strftime('%d.%m.%Y') == date:
+                    words_der_day += 1
+                if word[4]:
+                    if word[4].strftime('%d.%m.%Y') == date:
+                        familiar_words_per_day += 1
+            add_data['добавленные слова за день'].append(words_der_day)
+            accumulated_words_added = sum(add_data['добавленные слова за день'])
+            add_data['добавленные слова'].append(accumulated_words_added)
+            add_data['изученные слова за день'].append(familiar_words_per_day)
+            accumulated_familiar_words_per_day = sum(add_data['изученные слова за день'])
+            add_data['изученные слова'].append(accumulated_familiar_words_per_day)
+        return(add_data)
+    return {}
 
 
 def new_engwords_accumulated(username):
     sns.set(style="ticks", palette="colorblind", font="Arial")
-    df = pd.DataFrame(engword_dataframe(username))
-    new_engwords_accumulation = sns.relplot(
-        x="дни",
-        y="добавленные слова",
-        kind="line",
-        data=df,
-        height=6,
-        aspect=1
-        )
-    new_engwords_accumulation.fig.autofmt_xdate()
-    new_engwords_accumulation.set_xticklabels(rotation=90, horizontalalignment='right')
-    plt.savefig('webapp/media/progress/new_engwords_accumulated.png')
+    df_dict = engword_dataframe(username)
+    if df_dict:
+        df = pd.DataFrame(df_dict)
+        new_engwords_accumulation = sns.relplot(
+            x="дни",
+            y="добавленные слова",
+            kind="line",
+            data=df,
+            height=6,
+            aspect=1
+            )
+        new_engwords_accumulation.fig.autofmt_xdate()
+        new_engwords_accumulation.set_xticklabels(rotation=90, horizontalalignment='center')
+        plt.savefig(f'webapp/static/progress/new_engwords_{username}.png')
+        return f'progress/new_engwords_{username}.png'
+    return None
+
+
+def familiar_engwords_accumulated(username):
+    sns.set(style="ticks", palette="colorblind", font="Arial")
+    df_dict = engword_dataframe(username)
+    if df_dict:
+        if sum(df_dict['изученные слова']):
+            df = pd.DataFrame(df_dict)
+            familiar_engwords_accumulation = sns.relplot(
+                x="дни",
+                y="изученные слова",
+                kind="line",
+                data=df,
+                height=6,
+                aspect=1
+                )
+            familiar_engwords_accumulation.fig.autofmt_xdate()
+            familiar_engwords_accumulation.set_xticklabels(rotation=90, horizontalalignment='center')
+            plt.savefig(f'webapp/static/progress/familiar_engwords_{username}.png')
+            return f'progress/familiar_engwords_{username}.png'
+        return None
+    return None
+
+
+def difficult_engwords_catplot(username):
+    username = User.query.filter_by(username=username).first_or_404()
+    userdict = process_user_engdict_index(username)
+    failure_dict = {'слова': [], 'неудачные попытки': []}
+    if userdict:
+        for word in userdict:
+            failures = 0
+            # убрать повторения, рефакторинг
+            if word[5]:
+                failures += word[5]
+            if word[6]:
+                failures += word[6]
+            if word[7]:
+                failures += word[7]
+            if word[8]:
+                failures += word[8]
+            if word[9]:
+                failures += word[9]
+            if failures:
+                failure_dict['слова'].append(word[0].word_itself)
+                failure_dict['неудачные попытки'].append(failures)
+        df = pd.DataFrame(failure_dict)
+        df = df.sort_values('неудачные попытки')
+        df = df.tail(20)
+        # когда слов станет много, надо будет выводить не все, а, например, первые 20
+        sns.set(style="ticks", palette="colorblind", font="Arial")
+        difficult_engwords = sns.catplot(
+                x="слова",
+                y="неудачные попытки",
+                kind="bar",
+                data=df,
+                height=6,
+                aspect=1
+                )
+        difficult_engwords.fig.autofmt_xdate()
+        difficult_engwords.set_xticklabels(rotation=90, horizontalalignment='center')
+        plt.savefig(f'webapp/static/progress/difficult_engwords_{username.username}.png')
+        return f'progress/difficult_engwords_{username.username}.png'
+    return None
--- webapp/progress/views.py
+++ webapp/progress/views.py
@@ -3,6 +3,9 @@ from flask_login import current_user
 import locale
 import platform
 
+from webapp.progress.diagrams_functions import difficult_engwords_catplot
+from webapp.progress.diagrams_functions import familiar_engwords_accumulated
+from webapp.progress.diagrams_functions import new_engwords_accumulated
 from webapp.user.views import check_teacher_student
 
 blueprint = Blueprint('progress', __name__, url_prefix='/progress')
@@ -18,3 +21,45 @@ def choose_diagram(username):
     user_id = current_user.get_id()
     user_status = check_teacher_student(user_id)
     return render_template('progress/choose_diagram.html', user_status=user_status)
+
+
+@blueprint.route('/new_engwords/<username>')
+def new_engwords(username):
+    title = 'Рост числа английских слов в вашем словаре'
+    user_id = current_user.get_id()
+    user_status = check_teacher_student(user_id)
+    diagram = new_engwords_accumulated(username)
+    return render_template(
+        'progress/new_engwords.html',
+        page_title=title,
+        diagram=diagram,
+        user_status=user_status
+        )
+
+
+@blueprint.route('/familiar_engwords/<username>')
+def familiar_engwords(username):
+    title = 'Рост числа изученных английских слов '
+    user_id = current_user.get_id()
+    user_status = check_teacher_student(user_id)
+    diagram = familiar_engwords_accumulated(username)
+    return render_template(
+        'progress/familiar_engwords.html',
+        page_title=title,
+        diagram=diagram,
+        user_status=user_status
+        )
+
+
+@blueprint.route('/difficult_engwords/<username>')
+def difficult_engwords(username):
+    title = 'Самые сложные английские слова (не более 20 слов)'
+    user_id = current_user.get_id()
+    user_status = check_teacher_student(user_id)
+    diagram = difficult_engwords_catplot(username)
+    return render_template(
+        'progress/difficult_engwords.html',
+        page_title=title,
+        diagram=diagram,
+        user_status=user_status
+        )
